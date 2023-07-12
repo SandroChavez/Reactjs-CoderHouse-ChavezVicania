d68c76476972edd54f3d7a2bd21cd8333775a4ee
SandroChavez <126991806+SandroChavez@users.noreply.github.com>
2023-07-11 20:30:03 -0500

    Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
 import React from "react";
-import { useState } from 'react'
+import { useState } from "react";
 
-import  NavBar  from './componentes/NavBar/NavBar'
+import NavBar from "./componentes/NavBar/NavBar";
 import Logo from "./componentes/Logo/Logo";
-import  CartWidget  from './componentes/CartWidget/CartWidget'
+import CartWidget from "./componentes/CartWidget/CartWidget";
 import BarraBusqueda from "./componentes/BarraBusqueda/BarraBusqueda";
-import ItemListContainer from './componentes/ItemListContainer/ItemListContainer'
-import "./estilos/style.css"
+import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer";
+import "./estilos/style.css";
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <header>
-      <NavBar className={"NavBar"}>
-        <Logo className={"Icon"} />
-        <BarraBusqueda className={"BarraBusqueda"} />
-        <CartWidget className={"CartWidget"}/>
-      </NavBar>
-      <ItemListContainer texto={"Lista de productos"}/>
-    </header>
-  )
-}
+    <>
+      <header>
+        <div className="header-div" >
+          <NavBar className={"NavBar"}>
+            <Logo className={"Icon"} />
+            <BarraBusqueda className={"BarraBusqueda"} />
+            <CartWidget className={"CartWidget"} />
+          </NavBar>
+        </div>
+      </header>
+      <main>
+        <ItemListContainer texto={"Lista de juegos"} />
+      </main>
+    </>
+  );
+};
 
-export default App
+export default App;
