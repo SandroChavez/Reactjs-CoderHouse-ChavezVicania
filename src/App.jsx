518980622beddb2328e6065bcc8f4fcf4d486a4f
SandroChavez <126991806+SandroChavez@users.noreply.github.com>
2023-07-23 18:54:08 -0500

    algunos coambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState } from "react";
 
 import NavBar from "./componentes/NavBar/NavBar";
 import Logo from "./componentes/Logo/Logo";
@@ -9,8 +8,6 @@ import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer
 import "./estilos/style.css";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <header>
